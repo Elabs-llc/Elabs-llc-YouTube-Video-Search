86d9aacf5c27df5b862f19fcc4ef2b3e529eefc4
Edwin Asare <edwinasare08@gmail.com>
2024-08-09 11:47:36 -0700

    exception handled

--- search.py
+++ search.py
@@ -31,21 +31,24 @@ import random
 #### Ask user to enter a keyword or search term
 search_keyword= input("Enter a word to search: ")
 
-print(f'Searching YouTube for the search term {search_keyword}...')
+print(f'Searching YouTube for the search term {search_keyword.lower()}...')
 
 #### search for the search term on YouTube
 # Result will be displayed in html and we will use the urllib.request to get this
-html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword.replace(" ", "_"))
+html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword.replace(" ", "_").lower())
 
 ##### Results will be list of video ids
 # let's find all videos on youtube with the search word
+try:
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    # attach one video id to youtube link to open video
+    url = "https://www.youtube.com/watch?v=" + random.choice(video_ids)
 
-video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    urll = "https://www.youtube.com/watch?v=" + video_ids[0]
 
-# attach one video id to youtube link to open video
-url = "https://www.youtube.com/watch?v=" + random.choice(video_ids)
-
-urll = "https://www.youtube.com/watch?v=" + video_ids[0]
-
-# - Now open generated video ink in web browser
-webbrowser.open(url=urll)
+    # - Now open generated video ink in web browser
+    webbrowser.open(url=urll)
+    print('opening in your browser...')
+except:
+    print('Network error. Try again later!')
+    
